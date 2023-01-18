2c66c23fba5b01e5581ecc631505566ef64322e2
mochsandhyka <moch.sandhyka@gmail.com>
2023-01-18 14:30:20 +0700

    01-03

---
+++ 01-03/Fungsi.py
@@ -0,0 +1,15 @@
+def fungsiPengurangan():
+    a = 2 - 4
+    print(a)
+#fungsiPengurangan()
+
+def fungsiPembagian(x=20, y=5):
+    hasilBagi = x / y
+    print(hasilBagi)
+#fungsiPembagian(20,4)
+
+def fungsiPerkalian(x,y):
+    hasilKali = x * y
+    print(hasilKali)
+
+fungsiPerkalian(2,3)
--- 01-03/Perulangan.py
+++ 01-03/Perulangan.py
@@ -4,19 +4,34 @@
 #dapat menambah atau menghapus data melalui indeks atau tidak
 #menambahkan dengan perintah list.append
 listAngka = [1,2,3,16]
-listAngka2 = listAngka[2:]
+#merubah data dari index
 listAngka[0] = 0
+#menambah data
 listAngka.append(4)
+#menghapus data
 listAngka.remove(16)
 del listAngka[0]
+#Operator keanggotaan in dan not in akan mengembalikan nilai ke tipe data bool(TRUE OR FALSE)
+a = 3 in listAngka
+b = 3 not in listAngka
+#Perulangan
+print("[LIST ANGKA]")
+
+for la in listAngka:
+    print(la)
 
 
 #TUPLE
 #dapat diakses melalui indeks
 #slicing data misal ditampilkan dari data ke 2 - 3
 #tidak dapat menambah atau menghapus data melalui indeks atau tidak
-tupleAngka = (1,2,3)
-tupleAngka2 = tuple(listAngka)
+#tupleAngka = (1,2,3)
+#tupleAngka2 = tuple(listAngka)
+
+#perulangan
+#print("[TUPLE ANGKA]")
+#for ta in tupleAngka:
+ #   print(ta)
 
 
 #SET
@@ -24,9 +39,11 @@ tupleAngka2 = tuple(listAngka)
 #tidak ada fitur slicing data
 #dapat menambah atau menghapus data bukan melalui indeks
 #menambah dengan perintah set.add
-setAngka = {1,2,3}
-setAngka.add(4)
+#setAngka = {1,2,3}
+#setAngka.add(4)
 
+#perulangan
+#print("[SET ANGKA]")
+#for sa in setAngka:
+ #   print(sa)
 
-print("List angka :",listAngka)
-print("Set angka :",setAngka)
