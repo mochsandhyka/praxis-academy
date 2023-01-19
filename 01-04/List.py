687b2c94e9c4ad102322069b9a6f2e68b8fbd6ee
mochsandhyka <moch.sandhyka@gmail.com>
2023-01-19 14:45:55 +0700

    Update List.py

--- 01-04/List.py
+++ 01-04/List.py
@@ -15,7 +15,7 @@ for listK in listKendaraanTambahan:         #looping untuk menambah data di tamb
 #dictionary -> ditambahkan key saja
 
 #EXTEND
-listKendaraan = ["mobil","motor","truck"]  
+listKendaraan = ["mobil","motor","truck","motor"]  
 listKendaraanTuples = ("pesawat","kapal")
 listKendaraanSet = {"becak","delman"}
 listKendaraanString = "kereta"
@@ -23,14 +23,17 @@ listKendaraanDictionary = {
     "darat" : ["kereta","delman"],
     "udara" : "helikopter"}
 
-listKendaraanDic = list(listKendaraanDictionary.values())
-for i in listKendaraanDic:
-    listKendaraanDic.append(i)
-print(listKendaraanDic)
+listKendaraanDic = list(listKendaraanDictionary.values())   #convert value dictionary ke list 
+#for i in listKendaraanDic[0]:                               #looping isi listkendaraandic[0]
+ #   listKendaraanDic.append(i)                              #menambah listkendaraandic dengan isi dari listkendaraandic[0]
+#listKendaraanDic.pop(0)                                     #menghapus isi listkendaraandic index ke 0
 
-listKendaraanDic[0].append(listKendaraanDic[1])
-print(listKendaraanDic[0])
-print(listKendaraanDic)
+#listKendaraanDic.extend(listKendaraanDic[0])
+#listKendaraanDic.pop(0)
+
+
+#print(listKendaraanDic)
+
 
 #listKendaraan.extend(listKendaraanDictionary.values())
 #print(listKendaraan)
@@ -49,3 +52,32 @@ listKendaraan.insert(1,listKendaraanString)
 #print(listKendaraan)
 #listKendaraan.clear()           #remove all item 
 #print(listKendaraan)
+
+#INDEX
+index = listKendaraan.index("mobil")
+#print(index)
+
+#COUNT
+count = listKendaraan.count("motor")
+#print(count)
+
+#SORT
+listKendaraan.sort()            #sort a - z
+listKendaraan.sort(reverse=True)#sort z- a
+
+def fungsiSort(a):              #def fungsi sort berdasar banyak karakter
+    return len(a)
+
+listKendaraan.sort(key=fungsiSort)  #sort dengan fungsi
+
+kendaraan = [
+    {"merk" : "yamaha", "tahun" : 2005},
+    {"merk" : "bmw", "tahun" : 2020},
+    {"merk" : "honda",  "tahun" : 2002}
+]
+
+def fungsiSortTahun(a):
+    return a["tahun"]
+
+kendaraan.sort(reverse=False, key=fungsiSortTahun)
+
