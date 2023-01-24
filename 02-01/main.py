90a50bce5500f5107589b23b2cac988010b95e0f
mochsandhyka <moch.sandhyka@gmail.com>
2023-01-24 11:53:46 +0700

    02-02

--- 02-01/main.py
+++ 02-01/main.py
@@ -3,13 +3,15 @@
 #python3 -m venv venv
 #masuk ke . venv/bin/activate
 #pip install Flask
-from flask import Flask,request,redirect
+from flask import Flask,request,redirect,url_for,render_template
+
+listHewan = ["Ayam","Kelinci","Kuda"]
 
 app = Flask(__name__) 
 
 @app.route("/")
 def home():
-    return "Welcome to Home Page"
+    return render_template("index.html", content=listHewan)
 
 @app.route("/setting")
 def setting():
@@ -17,7 +19,8 @@ def setting():
 
 @app.route("/profile/<names>")
 def profile(names):
-    return f"Welcome to Profiles Page Mr/Mrs {names}"
+    return render_template("index2.html",content=names)
+
 
 @app.route("/profil/<int:id>")
 def profileId(id):
@@ -31,9 +34,11 @@ def query():
 #redirect
 @app.route("/redirect/")
 def redirectHome():
-    return redirect("/")
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return redirect(url_for("home"))
 
 
+#source venv/bin/activate
+#export FLASK_APP=main.py
+#flask run
+#if __name__ == '__main__':
+#    app.run(debug=True)
---
+++ 02-02/main.py
@@ -0,0 +1,15 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/register")
+def about():
+    return render_template("register.html")
+
+@app.route("/syarat")
+def syarat():
+    return render_template("syarat.html")
