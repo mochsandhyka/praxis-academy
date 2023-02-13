602f83c4f662f3e9480c35c37598ca79de90a0a5
mochsandhyka <moch.sandhyka@gmail.com>
2023-02-13 17:00:17 +0700

    05-01

--- 04-05/main.py
+++ 04-05/main.py
@@ -13,7 +13,6 @@ class TaskList(db.Entity):
 db.bind(provider="postgres", user="sandika", password="460446", host="localhost", database="todolist")
 db.generate_mapping(create_tables=True)
 Pony(app)
-
 @app.route("/")
 def view():
     data = []
@@ -26,15 +25,39 @@ def view():
     
     response = {
         "data": data,
-        "message": "ok"
+        "message": "data found"
     }
     return jsonify(response), HTTPStatus.OK
 
-@app.route("/create",methods=["POST"])
+# @app.route("/")
+# def view():
+#     try:
+#         data = []
+#         for i in db.execute("select *from tasklist"):
+#             data.append({
+#                 "id": i[0],
+#                 "task": i[1],
+#                 "description": i[2]
+#             })
+#         response = {
+#             "data": data,
+#             "message": "ok"
+#         }
+#         return jsonify(response), HTTPStatus.ok
+
+#     except Exception as response:
+#         response = {
+#             "data": "bad",
+#             "message": "bad gateway"
+#         }
+#         return jsonify(response),HTTPStatus.BAD_GATEWAY
+   
+
+@app.route("/",methods=["POST"])
 def create():
     bodyJson = request.json
     queryInsert = f"insert into tasklist(task, description) values('{bodyJson['task']}','{bodyJson['description']}')"
-    db.execute(queryInsert)
+    data = db.execute(queryInsert)
     response = {
         "data": "Created",
         "message": "Data inserted"
@@ -43,12 +66,35 @@ def create():
 
 @app.route("/delete/<id>",methods=['DELETE'])
 def delete(id):
-    db.execute(f"delete from tasklist where id = {id}")
-    response = {
-        "data": "delete tasklist",
-        "message": "delete success"
-    }
-    return jsonify(response), HTTPStatus.ACCEPTED
+    try:
+        selectById = (f"select id from tasklist where id = {id}")
+        data = []
+        for i in db.execute(selectById):
+            data.append({
+                "id": i[0]
+            })
+        if not data:
+            response = {
+                "data": "no data",
+                "message": "bad request"
+            }
+            return jsonify(response), HTTPStatus.BAD_REQUEST
+        else:
+            deleteById = (f"delete from tasklist where id = {id}")
+            db.execute(deleteById) 
+            response = {
+            "data": "delete tasklist",
+            "message": "delete success"
+        } 
+        return jsonify(response), HTTPStatus.ACCEPTED
+
+    except Exception as respError:
+        response={
+            "data": str(respError),
+            "message": "bad gateway"
+        }
+        return jsonify(response),HTTPStatus.BAD_GATEWAY
+ 
 
 @app.route("/update/<id>",methods=['PUT'])
 def update(id):
@@ -60,5 +106,35 @@ def update(id):
         "message": "Data updated"
     }
     return jsonify(response), HTTPStatus.ACCEPTED
+
+@app.route("/detail/<id>")
+def detail(id):
+    try:
+        querySelectById = db.execute(f"select task,description from tasklist where id = {id}")
+        data = []
+        for i in querySelectById:
+            data.append({
+                "task": i[0],
+                "description": i[1]
+            })
+
+        if not data:
+            respon = {
+                "data": "no data",
+                "message": "bad"
+            }
+            return jsonify(respon), HTTPStatus.BAD_REQUEST
+        respon = {
+            "data": data[0],
+            "message": "data is found"
+        }
+        return jsonify(respon),HTTPStatus.OK
+        
+    except Exception as respError:
+        respon = {
+            "data": str(respError),
+            "message": "bad gateway"
+        }
+        return jsonify(respon),HTTPStatus.BAD_GATEWAY
 
 
---
+++ 05/main.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template,redirect,flash
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField
+from wtforms.validators import DataRequired
+from pony.flask import Pony
+from pony.orm import Database,Required
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "a223jun98e23nce2j3ce23"
+db =Database()
+
+class User(db.Entity):
+    name = Required(str)
+    email = Required(email)
+    date_added = Required(datetime)
+
+class NameFormer(FlaskForm):
+    name = StringField("What's Your name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+@app.route("/")
+def home():
+    return "this is home"
+
+@app.route("/name",methods=['GET','POST'])
+def name():
+    name = None
+    form = NameFormer()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash("Form Submitted Successfully")
+    return render_template("name.html", name = name, form = form)
